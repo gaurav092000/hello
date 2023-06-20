e1b42f9a29590636c640024e5bfde9d8c1bbc4e2
gaurav2000 <gauravkawde@nimapinfotech.com>
2023-06-20 16:17:06 +0530

    tuesday

--- hello/Program.cs
+++ hello/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,9 +11,45 @@ namespace hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
-            Console.ReadLine();
+            //int g = 34;
+            //Console.WriteLine(g);
+            //DATA TYPE 
+            /*
+             integer= int gaurav;
+            floating
+             */
 
+            /* string np = Console.ReadLine();
+             Console.WriteLine(np);
+             //Console.WriteLine("Hello world");
+             //Console.WriteLine("gaurav kawde");
+             //Console.WriteLine("c#");
+             //heloo
+             /*
+              */
+
+            //int a = 10;
+            //float b = 10.3F;
+            //bool c = true;
+            //char c2 = 'r';
+            //string s = "this my computer";
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+            //Console.WriteLine(c2);
+            //Console.WriteLine(s);
+
+            //type casting
+            //explite
+            //int x = (int)6.5;
+            //double y = x;
+            //Console.WriteLine(y);
+            //Console.ReadLine();
+
+            float vrr = Convert.ToInt32(3.55);
+            Console.WriteLine(vrr);
+            Console.ReadLine();
+            
         }
     }
 }
