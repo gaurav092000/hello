781f08027d98ab9fd6a3522af4066aa14ea37722
gaurav2000 <gauravkawde@nimapinfotech.com>
2023-06-22 12:26:42 +0530

    Add project files.

---
+++ hello1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello1
+{
+    internal class Class1
+    {
+        static void Main()
+        {
+            Console.WriteLine(Class1.str);
+            Console.ReadKey();
+        }
+        static String str = "gaurav";
+
+    }
+}
