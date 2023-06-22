f17a266058f0c0974ad58efd8dc29b3211fa4a7e
gaurav092000 <137068757+gaurav092000@users.noreply.github.com>
2023-06-22 12:33:50 +0530

    Add files via upload
    thrusday

---
+++ hello1/hello1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.add();
+            program.sub();
+            Console.ReadKey();
+        }
+        int a, b = 10, c = 20;
+        public void add()
+        {
+            a = b + c;
+            Console.WriteLine("add: "+a);
+        }
+        public void sub()
+        {
+            a=c-b;
+            Console.WriteLine("sub :" +a);
+        }
+
+    }
+}
